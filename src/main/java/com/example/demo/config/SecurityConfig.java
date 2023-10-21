6575a037b50556a18ce46f4c24cbeb7cfba81381
superigera <super_rigera@icloud.com>
2023-10-21 16:57:43 +0900

    メンタ確認用

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import static org.springframework.security.config.Customizer.*;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,27 +25,32 @@ public class SecurityConfig {
 //		auth.inMemoryAuthentication().withUser("yama3").password(passwordEncoder().encode("123456")).roles("USER");
 //	}
 //
-//	@Bean
+//	@Autowired
 //	public BCryptPasswordEncoder passwordEncoder() {
 //		return new BCryptPasswordEncoder();
 //	}
 
+	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+//		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()) // ここでPasswordEncoderを指定
+//				.withUser("user").password(passwordEncoder().encode("test")).roles("USER");
+		auth.inMemoryAuthentication().withUser("user").password("test").roles("USER");
 	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		System.out.println("login設定");
 		http.cors(withDefaults()).csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/login"), // TODO
 				new AntPathRequestMatcher("/h2-console/**")))
 				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
 				.authorizeHttpRequests(authorizeRequests -> authorizeRequests
 						.requestMatchers(new AntPathRequestMatcher("/csrf-token")).permitAll()
 						.requestMatchers(new AntPathRequestMatcher("/newMemberRegistration/comfirm")).permitAll()
-						.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-						.requestMatchers(new AntPathRequestMatcher("/login")).permitAll().anyRequest().authenticated())
-				.formLogin(form -> form.loginPage("/login").permitAll());
+						.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll().anyRequest()
+						.authenticated())
+				.formLogin(form -> form.loginProcessingUrl("/login").failureHandler((request, response, exception) -> {
+					response.setStatus(HttpStatus.UNAUTHORIZED.value());
+				}));
+
 		return http.build();
 	}
 
